76c1f3aa818015b9a262e17e55098d9175dd8979
Daniel <danielmaximos2001@gmail.com>
2025-08-01 23:11:05 -0400

    Generic stack implementation

--- Stack.c
+++ Stack.c
@@ -1,66 +1,90 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <stdio.h>
+#include <string.h>
 
 
 typedef struct Node {
-	int data;
+	void* data;
 	struct Node* next;
 }Node;
 
 
 typedef struct Stack {
 	Node* top;
+	size_t data_size; //size of each element's data
 }Stack;
 
 
-Stack* create_stack(){
+Stack* create_stack(size_t data_size){
 	
-	Stack* s = (Stack*)malloc(sizeof(Stack));
+	Stack* s = (Stack*)malloc(sizeof(data_size));
 	if(s == NULL){
 		return NULL; //mem alloc failed
 	}
 
 	s->top = NULL;
+	s->data_size = data_size;
 	return s;
 
 }
 
-void push(Stack* s, int value){
+void push(Stack* s, void* value_ptr){
 	Node* new_node = (Node*)malloc(sizeof(Node));
-	if(new_node == NULL){
+	if(!new_node) return;
+	
+	new_node->data = malloc(s->data_size);
+	if(!new_node->data){
+		free(new_node);
 		return;
 	}
 	
-	new_node->data = value;
+	memcpy(new_node->data, value_ptr, s->data_size);
+	
 	new_node->next = s->top;
-	s->top = new_node;
 
+	s->top = new_node;	
 }
 
-int pop(Stack* s){
+void* pop(Stack* s){
 	
-	//find the node with value int? and free the mem?
+
 	if(s->top == NULL){
-		return INT_MIN; //stack underflow ...
+		return NULL; //stack underflow ...
 	}
 
 	Node* temp = s->top; //Save pointer to the top node
-	int value = temp->data; //Grab the value to return
-	s->top = temp->next; //Move the top pointer down to the next node
-	free(temp); //Free the memory of the old top node
+	void* data_copy = malloc(s->data_size);
+	if(!data_copy){
+		return NULL;
+	}
 
-	return value;
+	memcpy(data_copy, temp->data, s->data_size);
+
+	s->top = temp->next; 
+
+	free(temp->data);
+	free(temp);
+
+	return data_copy;
 
 }
 
-int peek(Stack* s){
+void* peek(Stack* s){
 
 	if(s->top == NULL){
-		return INT_MIN;
+		return NULL;
 	}
 	
-	return s->top->data;
+	void* data_copy = malloc(s->data_size);
+	
+	if(!data_copy){
+		return NULL;
+	}
+
+	memcpy(data_copy, s->top->data, s->data_size);
+
+	return data_copy;
 }
 
 int is_empty(Stack* s){
@@ -73,6 +97,7 @@ void free_stack(Stack* s){
 	Node* current = s->top;
 	while(current != NULL){
 		Node* next = current->next;
+		free(current->data);
 		free(current);
 		current = next;
 	}	
@@ -81,32 +106,38 @@ void free_stack(Stack* s){
 }
 
 
+/* test struct for generic stack implem. */
+
+typedef struct {
+	int x;
+	int y;
+} Point;
 
 
 int main(){
-
-	Stack *curr_stack = create_stack();
 	
-	push(curr_stack, 10);
-	push(curr_stack, 20);
-	push(curr_stack, 30);
+	Stack* point_stack = create_stack(sizeof(Point));
 
-	printf("Peek 1: %d\n",  peek(curr_stack));
-	printf("Pop 1: %d\n", pop(curr_stack));
-	printf("Peek 2: %d\n", peek(curr_stack));
+	Point p1 = {1, 2};
+	Point p2 = {3, 4};
+	Point p3 = {4, 5};
+
 	
-	printf("Is Empty? %d\n", is_empty(curr_stack));
-	
-	printf("Pop 2: %d\n", pop(curr_stack));
-	printf("Pop 3: %d\n", pop(curr_stack));
+	push(point_stack, &p1);
+	push(point_stack, &p2);
+	push(point_stack, &p3);
 
-	printf("Is Empty? %d\n", is_empty(curr_stack));
+	while(!is_empty(point_stack)){
+		Point* top = (Point*)peek(point_stack);
+		printf("Top: (%d, %d)\n", top->x, top->y);
 
-	free_stack(curr_stack);
+		Point* popped = (Point*)pop(point_stack);
+		printf("Popped: (%d, %d)\n", popped->x, popped->y);
+		free(popped);
 
+	}
+
+	free_stack(point_stack);
 	return 0;
-
-
-
 }
 
